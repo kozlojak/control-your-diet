289b9c5419533e67b98f36ea32bdb04d3b0128ec
kozlojak <jakub3008@gmail.com>
2018-07-05 11:11:13 +0200

    Add gradle specific files

---
+++ src/main/java/pl/jakubkozlowski/projects/controlyourdiet/ControlYourDietApplication.java
@@ -0,0 +1,12 @@
+package pl.jakubkozlowski.projects.controlyourdiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlYourDietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlYourDietApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/jakubkozlowski/projects/controlyourdiet/ControlYourDietApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.jakubkozlowski.projects.controlyourdiet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControlYourDietApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
